6abc316369ca8e6f901fed33058e98a9e55cac68
viktortuul <viktor.tuul@gmail.com>
2018-10-31 11:55:14 +0100

    updated path follower

--- ras_keyboard_control/src/path_follower.py
+++ ras_keyboard_control/src/path_follower.py
@@ -5,6 +5,7 @@ import math
 import matplotlib.pyplot as plt
 import rospy
 import std_msgs.msg
+from std_msgs.msg import String
 import geometry_msgs.msg
 from geometry_msgs.msg import Pose
 from nav_msgs.msg import Path
@@ -27,45 +28,29 @@ class State:
 class PathFollower():
     def __init__(self):
         self.D = 0.15  # look-ahead distance
-        self.L = 0.21  # 24 before
+        self.L = 0.21  # robot base length
 
         # current robot position (received by odom publisher continously)
         self.x_odom = 0.0
         self.y_odom = 0.0
         self.theta_odom = 0.0
 
-        '''
-        # current target position
-        self.x_target = 0.0
-        self.y_target = 0.0
-        self.theta_target = 0.0
-        '''
-
         # parameters
         self.follow_new_path = False
+        self.abort_path_following = False
 
         # current path
         self.path_x = []
         self.path_y = []
 
-    #####################################################
-    #             /robot_odom Callback          #
-    #####################################################
+    #####################################
+    #             Callbacks             #
+    #####################################
     def odomCallback(self, msg):
         self.x_odom = msg.pose.pose.position.x
         self.y_odom = msg.pose.pose.position.y
         (r, p, y) = tf.transformations.euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
         self.theta_odom = y
-
-    '''
-    def targetCallback(self, msg):
-        self.x_target = msg.position.x
-        self.y_target = msg.position.y
-        (r, p, y) = tf.transformations.euler_from_quaternion([msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w])
-        self.theta_target = y
-        print("path follow received new target", msg)
-        self.follow_new_path = True
-    '''
 
     def pathCallback(self, msg):
         path = msg.poses
@@ -76,17 +61,52 @@ class PathFollower():
         for i in range(len(path)):
             self.path_x.append(path[i].pose.position.y -0.2)
             self.path_y.append(-path[i].pose.position.x + 0.2)
+
+        # set flags to start executing 
         self.follow_new_path = True
+        self.abort_path_following = False
 
+    def flagCallback(self, msg):
+        flag = msg.data
+        if flag == "STOP":
+            self.abort_path_following = True
 
-    def send_message(self, LINEAR_VELOCITY, ANGULAR_VELOCITY):
+    #####################################
+    #             Publishers            #
+    #####################################
+    def send_velocity(self, LINEAR_VELOCITY, ANGULAR_VELOCITY):
         VEL = geometry_msgs.msg.Twist()
         VEL.linear.x = LINEAR_VELOCITY
         VEL.angular.z = ANGULAR_VELOCITY
         pub_path_following_VEL.publish(VEL)
+        
+    def send_flag(self, flag_message):
+        flag = std_msgs.msg.String()
+        flag.data = flag_message
+        pub_flag_done.publish(flag)
 
+    #####################################
+    #             Alogrithm             #
+    #####################################
+
+    def override_velocities(self, ang_vel_temp, alpha):
+        lin_vel = 0
+        ang_vel = ang_vel_temp
+
+        # motor controller commands
+        if alpha > 3.141/1.5:
+            lin_vel = 0
+            ang_vel = -0.8
+        elif alpha < -3.141/1.5:
+            lin_vel = 0
+            ang_vel = 0.8
+        else:
+            lin_vel = 0.08
+
+        return lin_vel, ang_vel
 
     def pure_pursuit_control(self, state, path_x, path_y, t_ind_prev):
+        # get the target index to steer against
         t_ind = self.calc_target_index(state, path_x, path_y)
 
         # if the previous target was further away on the path than now, use that instead
@@ -106,13 +126,11 @@ class PathFollower():
         alpha = math.atan2(ty - state.y, tx - state.x) - state.yaw
 
         # if reversing, flip the steering angle
-        #if state.v < 0:
+        # if state.v < 0:
         #    alpha = math.pi - alpha
 
-        D_tot = self.D
-
         # calculate an appropriate steering angle
-        delta = math.atan2(2.0 * self.L * math.sin(alpha) / D_tot, 1.0)
+        delta = math.atan2(2.0 * self.L * math.sin(alpha) / self.D, 1.0)
         return delta, t_ind, alpha
 
 
@@ -123,25 +141,22 @@ class PathFollower():
         d = [abs(math.sqrt(idx ** 2 + idy ** 2)) for (idx, idy) in zip(dx, dy)]
         t_ind = d.index(min(d))
 
-        # total look ahead distance (taking the speed into consideration)
-        D_tot = self.D
-
         # search length
-        L = 0.0
+        Ls = 0.0
 
         # find the index of the look head point on the path (look ahead is the distance ALONG the path, not straight line)
         path_length = len(path_x)
-        while L < D_tot and (t_ind + 1) < path_length:
+        while Ls < self.D and (t_ind + 1) < path_length:
             dx = path_x[t_ind + 1] - path_x[t_ind]
             dy = path_y[t_ind + 1] - path_y[t_ind]
-            L += math.sqrt(dx**2 + dy**2)
+            Ls += math.sqrt(dx**2 + dy**2)
             t_ind += 1
 
         return t_ind
 
     def main(self):
         while not rospy.is_shutdown():
-            # if follow_new_path == true --> reset and start following new path
+            # if follow_new_path == true --> start following new path
             if self.follow_new_path:    
                 self.follow_new_path = False
 
@@ -154,7 +169,12 @@ class PathFollower():
                 yaw = [state.yaw]
                 target_ind = self.calc_target_index(state, self.path_x, self.path_y)
 
+                # run the algorithm until the the target index reaches the last index in the path
                 while lastIndex > target_ind:
+                    # check if abort flag received
+                    if self.abort_path_following == True:
+                        break
+
                     # update state
                     state.x = self.x_odom
                     state.y = self.y_odom
@@ -167,20 +187,13 @@ class PathFollower():
                     # calculate appropriate steering
                     ang_vel, target_ind, alpha = self.pure_pursuit_control(state, self.path_x, self.path_y, target_ind)
 
-                    # motor controller commands
-                    if alpha > 3.141/1.5:
-                        lin_vel = 0
-                        ang_vel = -0.8
-                    elif alpha < -3.141/1.5:
-                        lin_vel = 0
-                        ang_vel = 0.8
-                    else:
-                        lin_vel = 0.08
+                    # override the velocities if needed (e.g. in a sharp turn)
+                    lin_vel, ang_vel = self.override_velocities(ang_vel, alpha)
 
-                    steering_gain = 1.0
+                    # send the velocity commands
+                    self.send_velocity(lin_vel, ang_vel)
 
-                    self.send_message(lin_vel, ang_vel*steering_gain)
-
+                    # animation below
                     x.append(self.x_odom)
                     y.append(self.y_odom)
                     yaw.append(self.theta_odom)
@@ -196,11 +209,11 @@ class PathFollower():
                         plt.pause(0.001)
 
                 # path follower done
-                self.send_message(0, 0) # stop robot
+                self.send_velocity(0, 0) # stop robot
 
-                flag = std_msgs.msg.String()
-                flag.data = "path_following_done"
-                pub_flag_done.publish(flag)
+                # send a flag to the state machine
+                self.send_flag("path_following_done")
+
 
             rate.sleep()
 
@@ -216,7 +229,7 @@ if __name__ == '__main__':
 
     # subscribers
     rospy.Subscriber("/robot_odom", Odometry, pf.odomCallback)
-    #rospy.Subscriber("/target_pose", Pose, pf.targetCallback)
     rospy.Subscriber("/aPath", Path, pf.pathCallback)
+    rospy.Subscriber("/path_follower_flag", String, pf.flagCallback)
 
     pf.main()
